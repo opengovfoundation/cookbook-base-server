73e72e5a513f305c8dc46e01ce29b4e9f902cd93
Bill Hunt <bill@krues8dr.com>
2016-06-21 10:29:39 -0400

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'base-server'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures base-server'
+long_description 'Installs/Configures base-server'
+version          '0.1.0'
